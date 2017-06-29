23eeb234256a69f30106d04fd7ed49b3fb355679
Enrico Franco <e.franco@zero11.it>
2017-06-29 14:13:02 +0200

    Job Application: Initial project

---
+++ Exam20150622_JobApplications/src/applications/ApplicationException.java
@@ -0,0 +1,8 @@
+package applications;
+
+@SuppressWarnings("serial")
+public class ApplicationException extends Exception {
+	public ApplicationException() {
+		super("ApplicationException");
+	}
+}
