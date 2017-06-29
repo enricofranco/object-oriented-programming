9f19ca38b7e0dca3be11d941a6aed4763a66748d
Enrico Franco <e.franco@zero11.it>
2017-06-29 10:05:01 +0200

    Restaurant: First solution attempt

--- Exam20160711_Restaurants/src/restaurantChain/Chain.java
+++ Exam20160711_Restaurants/src/restaurantChain/Chain.java
@@ -1,25 +1,44 @@
 package restaurantChain;
 
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class Chain {	
+public class Chain {
 
-		public void addRestaurant(String name, int tables) throws InvalidName {
-		}
-		
-		public Restaurant getRestaurant(String name) throws InvalidName {
-			return null;
-		}
-		
-		public List<Restaurant> sortByIncome() {
-			return null;
-		}
-		
-		public List<Restaurant> sortByRefused() {
-			return null;
-		}
-		
-		public List<Restaurant> sortByUnusedTables() {
-			return null;
-		}
+	private Map<String, Restaurant> restaurants = new HashMap<>();
+	
+	public void addRestaurant(String name, int tables) throws InvalidName {
+		Restaurant r = new Restaurant(name, tables);
+		if(restaurants.containsKey(name))
+			throw new InvalidName();
+		restaurants.put(name, r);
+	}
+
+	public Restaurant getRestaurant(String name) throws InvalidName {
+		Restaurant r = restaurants.get(name);
+		if(r == null)
+			throw new InvalidName();
+		return r;
+	}
+
+	public List<Restaurant> sortByIncome() {
+		return restaurants.values().stream()
+				.sorted(Comparator.comparing(Restaurant::getIncome).reversed())
+				.collect(Collectors.toList());
+	}
+
+	public List<Restaurant> sortByRefused() {
+		return restaurants.values().stream()
+				.sorted(Comparator.comparing(Restaurant::getRefused))
+				.collect(Collectors.toList());
+	}
+
+	public List<Restaurant> sortByUnusedTables() {
+		return restaurants.values().stream()
+				.sorted(Comparator.comparing(Restaurant::getUnusedTables))
+				.collect(Collectors.toList());
+	}
 }
--- Exam20160711_Restaurants/src/restaurantChain/Restaurant.java
+++ Exam20160711_Restaurants/src/restaurantChain/Restaurant.java
@@ -1,46 +1,104 @@
 package restaurantChain;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Restaurant {
 	
+	private String name;
+	private int tablesAvailable;
+	private int refusedCustomer = 0;
+	private double totalPayed = 0.0;
+	private Map<String, Double> menus = new HashMap<>();
+	private Map<String, Integer> groups = new HashMap<>();
+	private Map<String, LinkedList<String>> orderGroups = new HashMap<String, LinkedList<String>>();
+	private List<String> payedGroups = new LinkedList<>();
+	
+	public Restaurant(String name, int tablesAvailable) {
+		this.name = name;
+		this.tablesAvailable = tablesAvailable;
+	}
+
 	public String getName() {
-		return null;
+		return name;
 	}
-	
+
 	public void addMenu(String name, double price) throws InvalidName {
+		if(menus.containsKey(name))
+			throw new InvalidName();
+		menus.put(name, price);
 	}
-	
+
 	public int reserve(String name, int persons) throws InvalidName {
-		return -1;
+		if(groups.containsKey(name))
+			throw new InvalidName();
+		if(tablesAvailable*4 < persons) {
+			System.err.println("Prenotation rejected!");
+			refusedCustomer += persons;
+			return 0;
+		}
+		groups.put(name, persons);
+		int tablesBooked = (int) Math.ceil((double)persons/4);
+		tablesAvailable -= tablesBooked;
+		return tablesBooked;
 	}
-	
+
 	public int getRefused() {
-		return -1;
+		return refusedCustomer;
 	}
-	
+
 	public int getUnusedTables() {
-		return -1;
+		return tablesAvailable;
 	}
-	
+
 	public boolean order(String name, String... menu) throws InvalidName {
-		return false;
+		if(! groups.containsKey(name))
+			throw new InvalidName();
+		if(menu.length < groups.get(name)) {
+			System.err.println("Order not accepted");
+			return false;
+		}
+		LinkedList<String> menuList = new LinkedList<>();
+		for(String m : menu) {
+			if(! menus.containsKey(m))
+				throw new InvalidName();
+			menuList.add(m);
+		}
+		orderGroups.put(name, menuList);
+		return true;
 	}
-	
+
 	public List<String> getUnordered() {
-		return null;
+		return groups.keySet().stream()
+				.filter(g -> orderGroups.get(g) != null)
+				.sorted()
+				.collect(Collectors.toList());
 	}
-	
+
 	public double pay(String name) throws InvalidName {
-		return -1.0;
+		if(! groups.containsKey(name))
+			throw new InvalidName();
+		if(! orderGroups.containsKey(name))
+			throw new RuntimeException();
+		double amount = orderGroups.get(name).stream()
+				.mapToDouble(m -> menus.get(m))
+				.sum();
+		totalPayed += amount;
+		return amount;
 	}
-	
+
 	public List<String> getUnpaid() {
-		return null;
+		return groups.keySet().stream()
+				.filter(g -> ! payedGroups.contains(g))
+				.sorted()
+				.collect(Collectors.toList());
 	}
-	
+
 	public double getIncome() {
-		return -1.0;
+		return totalPayed;
 	}
 
 }
