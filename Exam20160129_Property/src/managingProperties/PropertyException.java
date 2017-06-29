a5ec4de8736d7d87d2b60d6d32074d985237b996
Enrico Franco <e.franco@zero11.it>
2017-06-29 16:09:07 +0200

    Property: Initial project

---
+++ Exam20160129_Property/src/managingProperties/PropertyException.java
@@ -0,0 +1,12 @@
+package managingProperties;
+
+public class PropertyException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public PropertyException() {
+	}
+
+	public PropertyException(String msg) {
+		super(msg);
+	}
+}
