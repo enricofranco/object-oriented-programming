6e83b3add4f1f8a164de1972ad536e59904dfc3b
Enrico Franco <e.franco@zero11.it>
2017-06-29 20:15:59 +0200

    Web Forum: Initial project

---
+++ WebForumProject/src/forum/DuplicateNickname.java
@@ -0,0 +1,11 @@
+package forum;
+
+@SuppressWarnings("serial")
+public class DuplicateNickname extends Exception {
+	public DuplicateNickname() {
+	}
+
+	public DuplicateNickname(String msg) {
+		super(msg);
+	}
+}
